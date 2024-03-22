f32184072a6778bc83f191f6978b69282a9c4295
Mary Xu <66391466+mxumary@users.noreply.github.com>
2024-03-22 11:11:35 -0400

    added a preliminary python script

---
+++ script.py
@@ -0,0 +1,19 @@
+import glob
+import subprocess
+
+audio_files = glob.glob("audio_files/set_two/*audio-*")
+print(audio_files)
+output_file = "output_amix.webm"
+# Create the input part of the command with `-i` flags for each audio file
+inputs = ' '.join(f'-i "{file}"' for file in audio_files)
+
+# The number of inputs for the amix filter
+num_inputs = len(audio_files)
+
+# Construct the filter_complex part
+filter_complex = f'amix=inputs={num_inputs}:duration=longest:dropout_transition=3' # first, longest, shortest duration
+
+# Assemble and return the full FFmpeg command
+command = f'ffmpeg {inputs} -filter_complex "{filter_complex}" {output_file}'
+print(command)
+subprocess.call(command, shell=True)
